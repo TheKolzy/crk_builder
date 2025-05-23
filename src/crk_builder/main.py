4dbb874b060f5ee98041cd60e3b69a891cbcd0f8
TheKolzy <kolzykun@gmail.com>
2025-05-23 18:36:16 +0200

    update window_manager

--- src/crk_builder/main.py
+++ src/crk_builder/main.py
@@ -1,7 +1,10 @@
 from utils.window_manager import WindowManager
 
+WINDOW_NAME: str = "BlueStacks App Player"
+
 def main() -> None:
-    WindowManager()
+    wm: WindowManager = WindowManager(WINDOW_NAME)
+    wm.configure_window()
 
 if __name__ == "__main__":
     main()
--- src/crk_builder/utils/window_manager.py
+++ src/crk_builder/utils/window_manager.py
@@ -1,3 +1,25 @@
+from pygetwindow import Win32Window
+from typing      import Optional
+
+import pygetwindow as pgw
+
 class WindowManager:
-    def __init__(self) -> None:
-        print("Hello, world 2!")
+    def __init__(self, window_name: str) -> None:
+        self.__window_name: str                   = window_name
+        self.__window     : Optional[Win32Window] = None
+
+    def configure_window(self) -> None:
+        if not self.__find_window():
+            return
+
+    def __find_window(self) -> bool:
+        # List of windows with this name
+        window_list = pgw.getWindowsWithTitle(self.__window_name)
+
+        if not window_list:
+            print(f"[Error]: The \"{self.__window_name}\" window has not been found.")
+            return False
+
+        # The first element in the list is the window
+        self.__window = window_list[0]
+        return True
